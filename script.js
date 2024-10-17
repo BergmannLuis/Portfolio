0c609627ff6549d381541caeb1f5c53234e092d3
LuisBergmann <luis.bergmann@fielmann.com>
2024-10-17 17:09:41 +0200

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active')
 }
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = {
+        full_name: document.getElementById('full_name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value,
+        phone_number: document.getElementById('phone_number').value
+    };
+
+    fetch('http://172.16.192.196/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+});
